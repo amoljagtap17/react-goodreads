0d26e20bcb953c593e432a36ea68d80bfb17dd0f
Amol Jagtap <ajfsdeveloper@gmail.com>
2019-04-08 01:41:32 +0530

    REACT-GOODREADS: Update Apollo URL on env value

--- client/src/apollo/client.js
+++ client/src/apollo/client.js
@@ -1,7 +1,9 @@
 import ApolloClient from 'apollo-boost'
 
+const uri = process.env.NODE_ENV === 'production' ? 'https://react-goodreads.herokuapp.com/' : 'http://localhost:4000/'
+
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/'
+  uri
 })
 
 export default client
