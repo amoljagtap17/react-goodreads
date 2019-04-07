f400abb8203a5740351297a16eec4428204bf613
Amol Jagtap <ajfsdeveloper@gmail.com>
2019-04-07 17:29:34 +0530

    REACT-GOODREADS: Pass Search String to BookList component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,27 +1,28 @@
 import React, { useState } from 'react'
 import { ApolloProvider } from 'react-apollo'
+import loadable from '@loadable/component'
 import client from '../apollo/client'
 import Header from './Header'
 import SearchBar from './SearchBar'
-import BookList from './BookList'
+// import BookList from './BookList'
+
+const BookList = loadable(() => import('./BookList'), {
+  fallback: <div>loading BookList component</div>
+})
 
 const App = () => {
 
-  const [showBookList, setShowBookList] = useState(false)
   const [searchTerm, setSearchTerm] = useState("")
-
-  const onSearchSubmit = () => {
-    setShowBookList(true)
-  }
 
   return (
     <ApolloProvider client={client}>
       <Header />
-      <SearchBar
-        searchTerm={searchTerm}
-        setSearchTerm={setSearchTerm}
-        onSearchSubmit={onSearchSubmit} />
-      {showBookList && <BookList searchTerm={searchTerm} />}
+      <SearchBar setSearchTerm={setSearchTerm} />
+      {
+        searchTerm !== "" &&
+        <BookList
+          searchTerm={searchTerm} />
+      }
     </ApolloProvider>
   )
 }
--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,28 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const SearchBar = ({ searchTerm, setSearchTerm, onSearchSubmit }) => (
-  <div className="grid-container">
-    <div className="grid-x grid-padding-x">
-      <div className="cell">
-        <fieldset className="fieldset">
-          <legend>Search Books by title</legend>
-          <div className="input-group">
-            <input
-              className="input-group-field"
-              type="text"
-              placeholder="Enter Search String"
-              value={searchTerm}
-              onChange={e => setSearchTerm(e.target.value)} />
-            <div className="input-group-button">
-              <button
-                className="button"
-                onClick={onSearchSubmit}>Search</button>
+const SearchBar = ({ setSearchTerm }) => {
+
+  const [searchText, setSearchText] = useState("")
+
+  return (
+    <div className="grid-container">
+      <div className="grid-x grid-padding-x">
+        <div className="cell">
+          <fieldset className="fieldset">
+            <legend>Search Books by title</legend>
+            <div className="input-group">
+              <input
+                className="input-group-field"
+                type="text"
+                placeholder="Enter Search String"
+                value={searchText}
+                onChange={e => setSearchText(e.target.value)} />
+              <div className="input-group-button">
+                <button
+                  className="button"
+                  onClick={() => setSearchTerm(searchText)}>Search</button>
+              </div>
             </div>
-          </div>
-        </fieldset>
+          </fieldset>
+        </div>
       </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default SearchBar
